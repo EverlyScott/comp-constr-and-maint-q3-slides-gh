8ac9caf82121343f0ec88af71f08e6c3dde7fb55
ScribbleNerd <contact@scribblenerd.com>
2022-02-08 14:00:23 -0600

    var

---
+++ var/node.js
@@ -0,0 +1,14 @@
+var variable = "string";
+
+console.log(variable); // Will print "string" to the console
+
+variable = "other string";
+
+console.log(variable); // Will print "other string" to the console
+
+// Using {} around any code puts that code in a new context
+{
+  var otherVariable = "third string";
+}
+
+console.log(otherVariable); // Will print "third string" to console, even though the variable was defined in a new context.
